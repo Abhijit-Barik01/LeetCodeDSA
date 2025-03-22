314e57985a3028514d415a3992cfc3789c3945c4
Abhijit Barik <abhijitcoder01@gmail.com>
2025-03-22 09:51:02 +0530

    Time: 14 ms (18.32%) | Memory: 140 MB (46.63%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        deque<int> flipQueue;
-        int count = 0;
+        int count  = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            while (!flipQueue.empty() && i > flipQueue.front() + 2) {
-                flipQueue.pop_front();
+        for(int i =2; i < nums.size(); i++)
+        {
+            if(nums[i-2] == 0) 
+            {
+                count ++;
+                nums[i-2] ^=1;
+                nums[i-1] ^=1;
+                nums[i] ^=1;
             }
 
-            if ((nums[i] + flipQueue.size()) % 2 == 0) {
-                if (i + 2 >= nums.size()) {
-                    return -1;
-                }
-                count++;
-                flipQueue.push_back(i);
-            }
         }
-
-        return count;
+        int sum =accumulate(nums.begin(), nums.end(),0);
+        if(sum == nums.size())
+            return count ;
+        return -1;
     }
 };
