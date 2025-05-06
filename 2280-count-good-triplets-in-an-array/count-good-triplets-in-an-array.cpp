ecd3720f7d4b10f3fe7bb24c503b9800ae73faed
Abhijit Barik <abhijitcoder01@gmail.com>
2025-05-06 15:14:36 +0530

    Time: 155 ms (33.04%) | Memory: 120.2 MB (30.35%) - LeetSync

--- 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
+++ 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
@@ -1,49 +1,83 @@
-class FenwickTree {
-private:
-    vector<int> tree;
-
-public:
-    FenwickTree(int size) : tree(size + 1, 0) {}
-
-    void update(int index, int delta) {
-        index++;
-        while (index < tree.size()) {
-            tree[index] += delta;
-            index += index & -index;
-        }
-    }
-
-    int query(int index) {
-        index++;
-        int res = 0;
-        while (index > 0) {
-            res += tree[index];
-            index -= index & -index;
-        }
-        return res;
-    }
-};
-
 class Solution {
 public:
+
+
+    void updateSegmentTree(int i, int l, int r, int idx, vector<long long>&segmentTree)
+    {
+        if( l == r)
+        {
+            segmentTree[i]=1;
+            return;
+
+        }
+
+        int mid = l +(r-l)/2;
+
+        if(idx <= mid)
+        {
+             updateSegmentTree(2*i+1,l,mid,idx,segmentTree);
+        }
+        else
+        {
+            updateSegmentTree(2*i+2,mid+1,r,idx,segmentTree);
+        }
+        segmentTree[i]= segmentTree[2*i+1] + segmentTree[2*i+2];   
+    }
+
+    long long querysegmentTree(int qs,int qe, int i, int l, int r, vector<long long>&segmentTree)
+    {
+        if(  l > qe || r < qs)
+        {
+            return 0;
+        }
+
+        if(l >=qs && r <=qe)
+        {
+            return segmentTree[i];
+        }
+
+        int  mid = l +(r-l)/2;
+
+        long long left  = querysegmentTree(qs,qe,2*i+1,l,mid,segmentTree);
+        long long right = querysegmentTree(qs,qe,2*i+2,mid+1,r,segmentTree);
+
+        return left+right;
+
+    }
+
+
+
     long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+
+
         int n = nums1.size();
-        vector<int> pos2(n), reversedIndexMapping(n);
-        for (int i = 0; i < n; i++) {
-            pos2[nums2[i]] = i;
+
+        unordered_map<int, int>mp;
+
+        for(int i = 0 ; i<n;i++)
+        {
+            mp[nums2[i]] = i; //storing index
         }
-        for (int i = 0; i < n; i++) {
-            reversedIndexMapping[pos2[nums1[i]]] = i;
+        //segment tree
+
+        vector<long long>segmentTree(4*n);
+        long long result =0 ;
+
+        updateSegmentTree(0,0,n-1,mp[nums1[0]],segmentTree);
+
+        for(int i=1; i< n; i++)
+        {
+            int idx = mp[nums1[i]];
+            long long  leftcommonCount=querysegmentTree(0,idx,0,0,n-1,segmentTree);
+
+            long long leftUnCommonCount = i - leftcommonCount;
+            long long elementAfterIdxNum2 = (n-1) - idx;
+            long long rightCommonCount = elementAfterIdxNum2 - leftUnCommonCount;
+            result += leftcommonCount*rightCommonCount;
+            updateSegmentTree(0,0,n-1,idx,segmentTree);
+
         }
-        FenwickTree tree(n);
-        long long res = 0;
-        for (int value = 0; value < n; value++) {
-            int pos = reversedIndexMapping[value];
-            int left = tree.query(pos);
-            tree.update(pos, 1);
-            int right = (n - 1 - pos) - (value - left);
-            res += (long long)left * right;
-        }
-        return res;
+        return result;
+        
     }
 };
