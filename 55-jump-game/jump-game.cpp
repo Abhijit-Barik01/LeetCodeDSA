36b54270c2f3a46255b536732eda961da3fbf871
Abhijit Barik <abhijitcoder01@gmail.com>
2025-05-18 23:43:34 +0530

    Time: 589 ms (8.64%) | Memory: 55.7 MB (6.54%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-   int dp[10001];
-   bool solve(vector<int>& nums, int n, int idx )
-   {
-        if(idx >= n-1)
-        {
-            return true;
-        }
-        if(dp[idx] != -1)
-            return dp[idx];
+    bool solve(vector<int>& nums, int n, int idx, vector<int>& dp) {
+        if (idx >= n - 1) return true;
+        if (dp[idx] != -1) return dp[idx];
 
-        for(int i =1; i <= nums[idx] ; i++)
-        {
-            if(solve(nums,n,idx+i) == true)
-            {
+        for (int jump = 1; jump <= nums[idx]; ++jump) {
+            if (solve(nums, n, idx + jump, dp)) {
                 return dp[idx] = true;
             }
         }
 
         return dp[idx] = false;
-   }
-    bool canJump(vector<int>& nums) {
+    }
 
+    bool canJump(vector<int>& nums) {
         int n = nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,n,0);
+        vector<int> dp(n, -1); // dynamic dp with size n initialized to -1
+        return solve(nums, n, 0, dp);
     }
 };
