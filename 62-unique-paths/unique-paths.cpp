6447a5efd6e08f86bb0d80a4a256046e61fd8e62
Abhijit Barik <abhijitcoder01@gmail.com>
2025-05-25 19:44:30 +0530

    Time: 3 ms (8.69%) | Memory: 9.4 MB (33.09%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool check(int m, int n , int i,int j)
-    {
-        if(i >=m || j >=n)
-            return false;
-        return true;
-    }
-    int solve(int m, int n , int i,int j)
-    {
-        if(i==m-1 && j == n-1)
-        {
-            return 1;
-        }
-      
-       
-        if(!check(m,n,i,j))
-            return 0;
-        if(dp[i][j] != -1)
-                return dp[i][j];
-       dp[i][j]= solve(m,n,i+1,j)+solve(m,n,i,j+1);
-        
-        return dp[i][j];
-
-    }
     int uniquePaths(int m, int n) {
-        dp.assign(m+1,vector<int>(n+1,-1));
-        return solve(m,n,0,0);
-        
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // Initialize the first row and first column
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+
+        // Fill the DP table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
+
+
