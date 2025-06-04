9ee80ee3d2c7194c165efe08a1149329173b225a
Abhijit Barik <abhijitcoder01@gmail.com>
2025-06-05 00:49:00 +0530

    Time: 2 ms (10.32%) | Memory: 8.7 MB (58.09%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int ans=-1;
-        int k=0;  
-        int niddilesize=needle.size();
-        while(k<haystack.size()){
-        // Finding first character of needle in haystack .
-        auto itr=find(haystack.begin()+k,haystack.end(),needle[0]);
-        int n=itr-haystack.begin();
-        // If first character is found extract its index , compare it with substring of haystick . 
-        string str=haystack.substr(n,niddilesize);
-       // If substring is equal to needle then this is our target so return its location.
-        if(str==needle){
-           ans=n; 
-            break;
+
+        int n = haystack.size();
+        int m = needle.size();
+
+        for(int winStart = 0 ; winStart <=n-m ; winStart++)
+        {
+            for(int i =0 ; i< m; i++)
+            {
+                if(needle[i] != haystack[i+winStart])
+                    break;
+            
+
+            if(i == m-1)
+                return winStart;
+
         }
-       else 
-       // If not then skip this and go ahead . now find needle[0] from one index greater than needle[0].
-           k=k+1;
-           continue;
-         }
-        
-        if(ans==-1)
-            return -1;
-        else
-            return ans;
+    }
+       return -1;
     }
 };
